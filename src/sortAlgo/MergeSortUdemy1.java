efb54ee3e1a9bd8a331c5e466aed22e1dec8aff5
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-06-13 10:56:15 +0700

    learn Merge Sort

---
+++ src/sortAlgo/MergeSortUdemy1.java
@@ -0,0 +1,89 @@
+package sortAlgo;
+
+import java.util.Arrays;
+
+public class MergeSortUdemy1 {
+    public static int[] merge(int[] array1, int[] array2) {
+        int[] combined = new int[array1.length + array2.length];
+        int index = 0;
+        int i = 0;
+        int j = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                combined[index] = array1[i];
+                index++;
+                i++;
+            } else {
+                combined[index] = array2[j];
+                index++;
+                j++;
+            }
+        }
+        while (i < array1.length) {
+            combined[index] = array1[i];
+            index++;
+            i++;
+        }
+        while (j < array2.length) {
+            combined[index] = array2[j];
+            index++;
+            j++;
+        }
+        return combined;
+    }
+
+    public static int[] mergeSortUdemy(int[] array) {
+        if (array.length == 1) return array;
+
+        int midIndex = array.length/2;
+        int[] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
+        int[] right = mergeSort(Arrays.copyOfRange(array, midIndex, array.length));
+
+        return merge(left, right);
+    }
+
+    public static int[] mergeSort(int[] array) {
+
+        if (array.length <= 1) return array;
+
+        int mid = array.length/2;
+        int r = 0;
+
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = array[i];
+        }
+
+        for (int i = mid; i < array.length; i++) {
+            right[r] = array[i];
+            r++;
+        }
+
+        return merge(mergeSort(left),mergeSort(right));
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] originalArray = {3,1,4,2};
+
+        int [] sortedArray = mergeSort(originalArray);
+
+        System.out.println( "Original Array: " + Arrays.toString( originalArray ) );
+
+        System.out.println( "\nSorted Array: " + Arrays.toString( sortedArray ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Original Array: [3, 1, 4, 2]
+
+            Sorted Array: [1, 2, 3, 4]
+
+         */
+
+    }
+}
