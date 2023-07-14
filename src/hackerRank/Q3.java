24be1094e3cc2104e6bc503a9fea638ffd508c0d
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-07-14 19:15:22 +0700

    jump with fix k but choose any position in array

--- src/hackerRank/Q3.java
+++ src/hackerRank/Q3.java
@@ -1,34 +1,100 @@
 package hackerRank;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Q3 {
     public static void main(String[] args) {
-        System.out.println(gainMaxValue(List.of(2,-3,4,6,1),2));
+
+        System.out.println(gainMaxValue(List.of(1),2));
         System.out.println(gainMaxValueThang(List.of(2,-3,4,6,1),2));
+        System.out.println(gainMaxValueThang(List.of(2,5,-8,-6,-7),3));
+        System.out.println(gainMaxValueThang(List.of(1),3));
+
+        System.out.println(gainMaxValue2(List.of(2,5,-8,-6,-7),3));
+
+        System.out.println(gainMaxValue2(List.of(2,-3,4,6,1),2));
+
+        System.out.println(gainMaxValue2(List.of(1),2));
+
+        System.out.println(gainMaxValueGPTRecursion(List.of(2,5,-8,-6,-7),3));
+
+        System.out.println(gainMaxValueGPT(List.of(2,-3,4,6,1),2));
+        System.out.println(gainMaxValueGPTRecursion(List.of(2,-3,4,6,1),2));
     }
 
+    public static int gainMaxValueGPTRecursion(List<Integer> security, int k) {
+        return recursionJump(0, security, k);
+    }
+
+    public static int recursionJump(int index, List<Integer> arr, int k) {
+        if (index >= arr.size()) {
+            return 0;
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = index; i < Math.min(index + k, arr.size()); i++) {
+            int sum = recursionJump(i + k, arr, k) + arr.get(i);
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+
+    public static int gainMaxValueGPT(List<Integer> security, int k) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return memoizedRecursionJumpGPT(0, security, k, memo);
+    }
+
+    public static int memoizedRecursionJumpGPT(int index, List<Integer> arr, int k, Map<Integer, Integer> memo) {
+        if (index >= arr.size()) {
+            return 0;
+        }
+        if (memo.containsKey(index)) {
+            return memo.get(index);
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = index; i < Math.min(index + k, arr.size()); i++) {
+            int sum = memoizedRecursionJumpGPT(i + k, arr, k, memo) + arr.get(i);
+            maxSum = Math.max(maxSum, sum);
+        }
+        memo.put(index, maxSum);
+        return maxSum;
+    }
+
+    public static int maxJumpSum(List<Integer> arr, int k) {
+        int n = arr.size();
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j += k) {
+                sum += arr.get(j);
+            }
+            maxSum = Math.max(sum,maxSum);
+        }
+        return maxSum;
+    }
+
+
+
     public static int gainMaxValueThang(List<Integer> security, int k) {
-        int sum = 0;
 
         List<Integer> sumList = new ArrayList<>();
 
-//        recursionJump(0, security, k, sum, sumList);
-        System.out.println(sumList);
+        recursionJump(0, security, k, sumList);
 
         return Collections.max(sumList);
     }
 
-    public static int recursionJump(int index, List<Integer> arr) {
-        if (index == 0)
-
-
-
-
+    public static void recursionJump(int index, List<Integer> arr,
+                                     int k, List<Integer>sumList) {
+        if (index >= arr.size()) return;
+        int sum = 0;
+        for (int i = index; i < arr.size(); i += k) {
+            sum += arr.get(i);
+        }
+        sumList.add(sum);
+        recursionJump(index + 1, arr, k, sumList);
     }
+
+
 
     public static int gainMaxValue(List<Integer> security, int k) {
 //        System.out.println(security);
@@ -58,5 +124,22 @@ public class Q3 {
 
         gain.add(tong);
         recursion(security,gain, k, ++start);
+    }
+
+    public static int gainMaxValue2(List<Integer> security, int k) {
+        if(security.size() == 1) {
+            return security.get(0);
+        }
+        int[] gain = new int[security.size()];
+        gain[0] = security.get(0);
+        gain[1] = Math.max(security.get(0), security.get(1));
+        for(int i = 2;  i <= security.size()-1 ; i++) {
+            if( i-k < 0) {
+                gain[i] = security.get(i);
+            } else {
+                gain[i] = Math.max(gain[i-k] + security.get(i), security.get(i));
+            }
+        }
+        return gain[gain.length-1];
     }
 }
---
+++ src/hackerRank/Q3Mot.java
@@ -0,0 +1,36 @@
+package hackerRank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q3Mot {
+    public static void main(String[] args) {
+        System.out.println(gainMaxValueGPTRecursion(List.of(2,-3,4,6,1),2));
+        System.out.println(gainMaxValueGPTRecursion(List.of(2,5,-8,-6,-7),3));
+        System.out.println(gainMaxValueGPTRecursion(List.of(1),2));
+    }
+    public static int gainMaxValueGPTRecursion(List<Integer> security, int k) {
+        int index = security.size() -1;
+        int[] dp = new int[index+1];
+        dp[index] = security.get(index);
+        return recursionJump(index,security,k, dp);
+    }
+
+
+
+    public static int recursionJump(int index, List<Integer> arr, int k, int[] dp) {
+
+
+        for (int i = index -1; i >= 0; i--) {
+            if (i+k >= arr.size()) {
+                dp[i] = arr.get(i);
+
+            } else {
+                dp[i] = dp[i+k] + arr.get(i);
+
+            }
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+}
