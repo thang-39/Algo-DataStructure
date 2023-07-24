b583f65228b44dec5476371d9e004a582fd2cdf0
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-07-24 21:44:11 +0700

    ok

--- src/takeuforward/dp/Lec12MaxFallingPathSum.java
+++ src/takeuforward/dp/Lec12MaxFallingPathSum.java
@@ -1,2 +1,141 @@
-package takeuforward.dp;public class Lec12MaxFallingPathSum {
+package takeuforward.dp;
+
+import java.util.Arrays;
+
+public class Lec12MaxFallingPathSum {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1,2,10,4},{100,3,2,1},{1,1,20,2}, {1,2,2,1}};
+        int[][] matrix2 = {{10,2,3}, {3,7,2},{8,1,5}};
+
+        System.out.println(getMaxPathSum(matrix1));
+        System.out.println(getMaxPathSum(matrix2));
+
+        System.out.println(getMaxPathSumMemorization(matrix1));
+        System.out.println(getMaxPathSumMemorization(matrix2));
+
+    }
+
+    public static int getMaxPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int result = Integer.MIN_VALUE;
+
+        for (int col = 0; col < matrix[0].length; col++) {
+
+//            result = Math.max(result, recursion(0,col,matrix));
+//            result = Math.max(result, recursion2(n-1,col,matrix));
+//            int temp = recursion3(n-1,col,matrix);
+//            result = Math.max(result, temp);
+            result = Math.max(result,recursion4(0,col,matrix));
+
+        }
+        return result;
+    }
+
+    private static int recursion(int row, int col, int[][] matrix) {
+
+        if (col < 0 || col >= matrix[0].length)
+            return -(int) Math.pow(10,9);
+
+        if (row == matrix.length - 1)
+            return matrix[row][col];
+
+
+
+        int up = matrix[row][col] + recursion(row+1,col,matrix);
+
+        int leftDiagonal = matrix[row][col] + recursion(row+1,col-1,matrix);
+
+        int rightDiagonal = matrix[row][col] + recursion(row+1,col+1,matrix);
+
+        return Math.max(up,Math.max(leftDiagonal,rightDiagonal));
+    }
+
+    private static int recursion2(int row, int col, int[][] matrix) {
+
+        if (col < 0 || col >= matrix[0].length)
+            return -(int) Math.pow(10,9);
+
+        if (row == 0)
+            return matrix[0][col];
+
+        int up = matrix[row][col] + recursion2(row-1,col,matrix);
+
+        int leftDiagonal = matrix[row][col] + recursion2(row-1,col-1,matrix);
+
+        int rightDiagonal = matrix[row][col] + recursion2(row-1,col+1,matrix);
+
+        return Math.max(up,Math.max(leftDiagonal,rightDiagonal));
+    }
+
+    private static int recursion3(int row, int col, int[][] matrix) {
+
+        if (row == 0)
+            return matrix[0][col];
+
+        int up = matrix[row][col] + recursion3(row-1,col,matrix);
+
+        int leftDiagonal = - (int) Math.pow(10,9);
+        if (col > 0)
+            leftDiagonal = matrix[row][col] + recursion3(row-1,col-1,matrix);
+
+        int rightDiagonal = - (int) Math.pow(10,9);
+        if (col < matrix[0].length-1)
+            rightDiagonal = matrix[row][col] + recursion3(row-1,col+1,matrix);
+
+        return Math.max(up,Math.max(leftDiagonal,rightDiagonal));
+    }
+
+    private static int recursion4(int row, int col, int[][] matrix) {
+        if (row == matrix.length - 1) return matrix[row][col];
+
+        int down = matrix[row][col] + recursion4(row+1,col,matrix);
+
+        int leftDiagonal = - (int) Math.pow(10,8);
+        if (col > 0)
+            leftDiagonal = matrix[row][col] + recursion4(row+1,col-1,matrix);
+
+        int rightDiagonal = - (int) Math.pow(10,8);
+        if (col < matrix[0].length - 1)
+            rightDiagonal = matrix[row][col] + recursion4(row+1,col+1,matrix);
+
+        return Math.max(down,Math.max(leftDiagonal,rightDiagonal));
+    }
+
+
+
+    public static int getMaxPathSumMemorization(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] dp = new int[n][m];
+
+        int result = Integer.MIN_VALUE;
+        for (int col = 0; col < m; col++) {
+//            result = Math.max(result, recursion2(n-1,col,matrix));
+            result = Math.max(result, recursionMemorization(0,col,matrix,dp));
+        }
+        return result;
+    }
+
+    private static int recursionMemorization(int row, int col, int[][] matrix, int[][] dp ) {
+
+        if (col < 0 || col >= matrix[0].length)
+            return -(int) Math.pow(10,9);
+
+        if (row == matrix.length - 1)
+            return matrix[row][col];
+
+        if (dp[row][col] != 0) return dp[row][col];
+
+        int up = matrix[row][col] + recursionMemorization(row+1,col,matrix,dp);
+
+        int leftDiagonal = matrix[row][col] + recursionMemorization(row+1,col-1,matrix,dp);
+
+        int rightDiagonal = matrix[row][col] + recursionMemorization(row+1,col+1,matrix,dp);
+
+        return dp[row][col] = Math.max(up,Math.max(leftDiagonal,rightDiagonal));
+    }
+
 }
--- src/takeuforward/dp/Lec12MaxFallingPathSumLaptop.java
+++ src/takeuforward/dp/Lec12MaxFallingPathSumLaptop.java
@@ -6,28 +6,28 @@ public class Lec12MaxFallingPathSumLaptop {
     public static void main(String[] args) {
         int[][] matrix1 = {{1,2,10,4},{100,3,2,1},{1,1,20,2},{1,2,2,1}};
         int[][] matrix2 = {{10,2,3}, {3,7,2},{8,1,5}};
-        System.out.println(getMaxPathSum(matrix1));
-        System.out.println(getMaxPathSum(matrix2));
+        System.out.println(getMaxPathSumLap(matrix1));
+        System.out.println(getMaxPathSumLap(matrix2));
     }
 
-    public static int getMaxPathSum(int[][] matrix) {
+    public static int getMaxPathSumLap(int[][] matrix) {
         int result = Integer.MIN_VALUE;
         for (int col = 0; col < matrix[0].length; col++) {
-            int temp = recursion(0, col, matrix);
+            int temp = recursionLap(0, col, matrix);
             result = Math.max(temp, result);
         }
         return result;
     }
 
-    private static int recursion(int row, int col, int[][] matrix) {
+    private static int recursionLap(int row, int col, int[][] matrix) {
         if (col < 0 || col == matrix[0].length) return - (int) Math.pow(10,8);
         if (row == matrix.length-1) return matrix[row][col];
 
-        int down = matrix[row][col] + recursion(row+1,col,matrix);
+        int down = matrix[row][col] + recursionLap(row+1,col,matrix);
 
-        int leftDiagonal = matrix[col][row] + recursion(row+1,col-1,matrix);
+        int leftDiagonal = matrix[row][col] + recursionLap(row+1,col-1,matrix);
 
-        int rightDiagonal = matrix[col][row] + recursion(row+1,col+1,matrix);
+        int rightDiagonal = matrix[row][col] + recursionLap(row+1,col+1,matrix);
 
         return Math.max(down, Math.max(leftDiagonal,rightDiagonal));
     }
