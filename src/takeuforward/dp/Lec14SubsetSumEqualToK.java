ebc9d80e62d62b44645b08fb645967c81c9c084f
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-07-27 10:36:12 +0700

    Subset sum equal to K

--- src/takeuforward/dp/Lec14SubsetSumEqualToK.java
+++ src/takeuforward/dp/Lec14SubsetSumEqualToK.java
@@ -13,6 +13,9 @@ public class Lec14SubsetSumEqualToK {
         System.out.println(subsetSumToKTabulation(4,5, new int[]{4,3,2,1}));
         System.out.println(subsetSumToKTabulation(5,4, new int[]{2,5,1,6,7}));
 
+        System.out.println(subsetSumToKTabulationSpaceOptimization(4,5, new int[]{4,3,2,1}));
+        System.out.println(subsetSumToKTabulationSpaceOptimization(5,4, new int[]{2,5,1,6,7}));
+
     }
 
     public static boolean subsetSumToK(int n, int k, int arr[]){
@@ -21,7 +24,7 @@ public class Lec14SubsetSumEqualToK {
 
     private static boolean recursion(int index, int target, int[] arr) {
         if (target == 0) return true;
-        if (index == 0) return arr[index] == target;
+        if (index == 0) return arr[0] == target;
 
         boolean notTake = recursion(index-1,target,arr);
 
@@ -55,7 +58,7 @@ public class Lec14SubsetSumEqualToK {
 
     private static boolean recursion(int index, int target, int[] arr, int[][] dp) {
         if (target == 0) return true;
-        if (index == 0) return arr[index] == target;
+        if (index == 0) return arr[0] == target;
         if (dp[index][target] != -1) {
             return dp[index][target] == 1;
         }
@@ -76,6 +79,8 @@ public class Lec14SubsetSumEqualToK {
     public static boolean subsetSumToKTabulation(int n, int k, int arr[]){
         boolean[][] dp = new boolean[n][k+1];
 
+        // for every index - target = 0 -> dp[i][0] = true
+        // (always true) - what are you looking for???
         for (int i = 0; i < n; i++) dp[i][0] = true;
 
         if (arr[0] <= k)
@@ -94,6 +99,34 @@ public class Lec14SubsetSumEqualToK {
 
         }
         return dp[n-1][k];
+    }
+
+    public static boolean subsetSumToKTabulationSpaceOptimization(int n, int k, int arr[]){
+        boolean[] dp = new boolean[k+1];
+
+        // for every index - target = 0 -> dp[i][0] = true
+        // (always true) - what are you looking for???
+        dp[0] = true;
+
+        if (arr[0] <= k)
+            dp[arr[0]] = true;
+
+        for (int ind = 1; ind < n; ind++) {
+            boolean[] temp = new boolean[k+1];
+//            temp[0] = true;
+            for (int target = 1; target <= k; target++) {
+                boolean notTake = dp[target];
+
+                boolean take = false;
+                if (arr[ind] <= target) {
+                    take = dp[target-arr[ind]];
+                }
+                temp[target] = take || notTake;
+            }
+            dp = temp;
+
+        }
+        return dp[k];
     }
 }
 
